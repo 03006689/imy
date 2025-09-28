ab51c60593ea2649a3e9d6489c2414cac8204d82
03006689 <03006689@live.napier.ac.uk>
2025-09-28 11:16:48 +0100

    latest

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,7 +2,7 @@ package com.napier.sem;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("boo yahllllla");
+        System.out.println("This is coursework");
 
 
     }
