36204e57aaa505910b8e23d9307c7192789bceb7
03006689 <03006689@live.napier.ac.uk>
2025-10-11 00:54:47 +0100

    workflow

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,4 +1,4 @@
-package  com.napier.sem;
+package com.napier.sem;
 
 import java.sql.*;
 
@@ -22,13 +22,17 @@ public class App {
             System.out.println("Connecting to database...");
             try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                Thread.sleep(5000); // shorter wait
+                // Connect to the world database
+                con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/world?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root",
+                        "example"
+                );
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println(" Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
@@ -42,77 +46,53 @@ public class App {
     public void disconnect() {
         if (con != null) {
             try {
-                // Close connection
                 con.close();
+                System.out.println("Disconnected from database.");
             } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
 
-    public Employee getEmployee(int ID)
-    {
-        try
-        {
-            // Create an SQL statement
+    /**
+     * Query: Show the first 10 cities.
+     */
+    public void showCountriesByPopulation() {
+        try {
             Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
-            if (rset.next())
-            {
-                Employee emp = new Employee();
-                emp.emp_no = rset.getInt("emp_no");
-                emp.first_name = rset.getString("first_name");
-                emp.last_name = rset.getString("last_name");
-                return emp;
+            String sql =
+                    "SELECT Code, Name, Continent, Region, Population " +
+                            "FROM country " +
+                            "ORDER BY Population DESC;";
+
+            ResultSet rset = stmt.executeQuery(sql);
+
+            System.out.println("\nAll countries in the world (largest to smallest population):\n");
+            System.out.printf("%-5s %-45s %-20s %-25s %-15s%n",
+                    "Code", "Name", "Continent", "Region", "Population");
+            System.out.println("=".repeat(115));
+
+            while (rset.next()) {
+                System.out.printf("%-5s %-45s %-20s %-25s %-15d%n",
+                        rset.getString("Code"),
+                        rset.getString("Name"),
+                        rset.getString("Continent"),
+                        rset.getString("Region"),
+                        rset.getInt("Population"));
             }
-            else
-                return null;
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
-            return null;
-        }
-    }
-    public void displayEmployee(Employee emp)
-    {
-        if (emp != null)
-        {
-            System.out.println(
-                    emp.emp_no + " "
-                            + emp.first_name + " "
-                            + emp.last_name + "\n"
-                            + emp.title + "\n"
-                            + "Salary:" + emp.salary + "\n"
-                            + emp.dept_name + "\n"
-                            + "Manager: " + emp.manager + "\n");
+
+        } catch (Exception e) {
+            System.out.println("Failed to retrieve countries: " + e.getMessage());
         }
     }
 
-/// //////////////////////////////////////////////////////
-
-public static void main(String[] args)
-{
-    // Create new Application
-    App a = new App();
-
-    // Connect to database
-    a.connect();
-    // Get Employee
-    Employee emp = a.getEmployee(255530);
-    // Display results
-    a.displayEmployee(emp);
-
-    // Disconnect from database
-    a.disconnect();
-}
+    /**
+     * Main entry point.
+     */
+    public static void main(String[] args) {
+        App a = new App();
+        a.connect();
+        a.showCountriesByPopulation();
+        a.disconnect();
+    }
 }
